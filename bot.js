bc9fda7aba730364631e58d965968e013f837dc0
yousefbots <45495875+yousefbots@users.noreply.github.com>
2019-08-03 19:20:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "!";
-var adminprefix = '!'
+var prefix = "$";
+var adminprefix = '$'
 
 const developers = ["445868371737575424","id"]
 client.on('message', message => {
